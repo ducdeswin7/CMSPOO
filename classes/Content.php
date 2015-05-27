e25bb17f535bc021e5d667e131c9c51ee58c3b51
ducdeswin7 <ducdeswin@outlook.com>
2015-05-27 11:25:44 +0200

    les classes

--- classes/Content.php
+++ classes/Content.php
@@ -4,4 +4,25 @@
 class Content
 {
 
+    
+    public function add()
+    {
+
+    }
+    public function delete()
+    {
+
+    }
+    public function update()
+    {
+
+    }
+    public function view()
+    {
+
+    }
+    public function liste()
+    {
+
+    }
 }
--- classes/ContentEntity.php
+++ classes/ContentEntity.php
@@ -3,5 +3,124 @@
 
 class ContentEntity
 {
+    protected $id;
+    protected $title;
+    protected $body;
+    protected $menuTitle;
+    protected $headerTitle;
+    protected $created_at;
+    protected $updated_at;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param mixed $body
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMenuTitle()
+    {
+        return $this->menuTitle;
+    }
+
+    /**
+     * @param mixed $menuTitle
+     */
+    public function setMenuTitle($menuTitle)
+    {
+        $this->menuTitle = $menuTitle;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHeaderTitle()
+    {
+        return $this->headerTitle;
+    }
+
+    /**
+     * @param mixed $headerTitle
+     */
+    public function setHeaderTitle($headerTitle)
+    {
+        $this->headerTitle = $headerTitle;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param mixed $created_at
+     */
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @param mixed $updated_at
+     */
+    public function setUpdatedAt($updated_at)
+    {
+        $this->updated_at = $updated_at;
+    }
 
 }
